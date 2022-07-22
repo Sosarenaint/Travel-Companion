a5c33841dc802eca4780e89bebea87de5d675cc4
Sosarenaint <93826368+Sosarenaint@users.noreply.github.com>
2022-07-22 14:33:54 +0400

    first commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# 🗺️ Travel Companion Web Application
+
+### _Travel companion app developed using React.js, RapidAPI, Material UI and Google Maps API_
+
+### :link: https://travel-companion-web.netlify.app/
+
+## Images
+
+<center><img src='./images/home.png' /></center>
+<br/>
+
+### Mobile
+
+<center><img src='./images/home-mobile.png' /></center>
+
+## Run Locally
+
+- Run this command `git clone https://github.com/sosarenaint/Travel-Companion.git`
+- You are now in the dev environment and you can play around
+- Search Might not work because \*Billing details not set in GCP
+
+## Technologies
+
+- React.qjs
+- RapidAPI
+- Material-UI
+- Google Maps API
---
+++ src/App.js
@@ -0,0 +1,98 @@
+import React, { useEffect, useState } from "react";
+
+// Material UI
+import { CssBaseline, Grid } from "@material-ui/core";
+
+// API
+import { getPlacesData, getWeatherData } from "./api";
+
+// Components
+import { Header, List, Map } from "./components";
+
+const App = () => {
+  const [childClicked, setChildClicked] = useState(null);
+  const [places, setPlaces] = useState([]);
+  const [weatherData, setWeatherData] = useState([]);
+
+  const [filteredPlaces, setFilteredPlaces] = useState([]);
+
+  const [type, setType] = useState("restaurants");
+  const [rating, setRating] = useState("");
+
+  const [coordinates, setCoordinates] = useState({});
+  const [bounds, setBounds] = useState({});
+
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(
+      ({ coords: { latitude, longitude } }) => {
+        setCoordinates({ lat: latitude, lng: longitude });
+      }
+    );
+  }, []);
+
+  useEffect(() => {
+    const filteredPlaces = places?.filter((place) => place?.rating > rating);
+    setFilteredPlaces(filteredPlaces);
+  }, [rating]);
+
+  useEffect(() => {
+    if (bounds?.sw) {
+      setIsLoading(true);
+
+      getWeatherData(coordinates.lat, coordinates.lng).then((data) =>
+        setWeatherData(data)
+      );
+
+      getPlacesData(type, bounds.sw, bounds.ne).then((data) => {
+        setPlaces(data?.filter((place) => place.name && place.num_reviews > 0));
+        setFilteredPlaces([]);
+        setIsLoading(false);
+      });
+    }
+  }, [bounds, type]);
+
+  return (
+    <>
+      <CssBaseline />
+      <Header setCoordinates={setCoordinates} />
+      <Grid
+        container
+        spacing={3}
+        style={{
+          width: "100%",
+          marginTop: "1rem",
+          margin: "0 auto",
+          height: "100%",
+        }}
+        justifyContent="center"
+        alignItems="center"
+      >
+        <Grid item xs={12} md={4}>
+          <List
+            places={filteredPlaces?.length ? filteredPlaces : places}
+            childClicked={childClicked}
+            isLoading={isLoading}
+            type={type}
+            setType={setType}
+            rating={rating}
+            setRating={setRating}
+          />
+        </Grid>
+        <Grid item xs={12} md={8}>
+          <Map
+            setCoordinates={setCoordinates}
+            setBounds={setBounds}
+            coordinates={coordinates}
+            places={filteredPlaces?.length ? filteredPlaces : places}
+            setChildClicked={setChildClicked}
+            weatherData={weatherData}
+          />
+        </Grid>
+      </Grid>
+    </>
+  );
+};
+
+export default App;
